b8f2a8833acff9a7b27d961ff8a530e32de96535
whiteAndBlackPanda <142206766+camachoRamel@users.noreply.github.com>
2025-09-19 07:09:54 +0800

    first commit

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>My First Laravel Project</title>
+</head>
+<body class="container mt-5">
+    <div class="card shadow p-4">
+        <h1 class="text-primary">Hello, Laravel World!</h1>
+        <p class="lead">This is my very first laravel page styled with Boostrap</p>
+        <a href="/" class="btn btn-outline-primary">Home</a>
+    </div>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\HelloController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return 'Hello world';
+});
+
+Route::get('/welcome', [HelloController::class, 'index']);
